f419a3f187fb4cb1d6322e32caffc9a7256b19af
urk23cs1262 <niveshr@karunya.edu.in>
2026-02-25 13:54:33 +0530

    fix tests for multiply function

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,5 @@
-from app import add
+from app import multiply
 
-def test_add():
-    assert add(2, 3) == 5
-    assert add(-1, 1) == 0
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(5, 0) == 0
