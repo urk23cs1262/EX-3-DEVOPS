b3bc85dd0c6add7e21ca59aa57222c208cc0cdfa
urk23cs1262 <niveshr@karunya.edu.in>
2026-02-25 13:49:44 +0530

    update app to use multiply function

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-def add(a, b):
-    return a + b
+def multiply(a, b):
+    return a * b
 
 if __name__ == "__main__":
     print("Hello from Python CI Lab!")
-    print("2 + 3 =", add(2, 3))
+    print("2 * 3 =", multiply(2, 3))
